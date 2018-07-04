8fb85f089d08dbb07b98488440ce3387eb088153
victordnt <victordnt@gmail.com>
2018-07-03 23:47:00 -0300

    Criação do segundo projeto

--- vue-cli-ex/src/App.vue
+++ vue-cli-ex/src/App.vue
@@ -1,12 +1,96 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="container component">
+
+    <div class="quoteMeter">
+      <h2>Quotes Added</h2>
+      <div class="healthbar">
+        <div class="healthbar text-center" style="background-color: steelblue; margin: 0; color: white; " :style="{width:quotes.length+'0%'}">
+          <p style="padding: 8px;">{{ quotes.length }}/10</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="writeQuote">
+      <div class="col-12">
+        <div class="m-auto col-8">
+
+          <label for="newQuote">
+            <h5>Quote</h5>
+          </label>
+          <textarea id="newQuote" style="width: 100%; height: 130px; " v-model="newQuote" @keyup.enter="addQuote"></textarea>
+
+          <div class="col-md-12">
+            <div class="text-center">
+              <button class="btn btn-primary" @click="addQuote">Add Quote</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="quotes col-md-12 ">
+      <div class="row m-2">
+        <app-quote v-for="quote in quotes" :quote="quote" @deleteQuote="del($event)"></app-quote>
+      </div>
+    </div>
+    <div class="col-12">
+      <div class="card text-white bg-info m-1">
+        <div class="card-header">Info:</div>
+        <div class="card-body quote text-center">
+          <p class="card-text" >Click on a quote to delete it.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
+  import Quote from "./components/Quote";
   export default {
+    data(){
+      return {
+        quotes:[],
+        newQuote:'',
+      }
+    },
+    methods:{
+      addQuote(){
+        let newQuote = this.newQuote.trim();
+        if(this.quotes.length>=10)
+          alert("You can only insert 10 quotes!");
+        if(newQuote && this.quotes.length<10){
+          this.quotes.push(newQuote);
+          this.newQuote = '';
+        }
+      },
+      del(q){
+        this.quotes.splice(this.quotes.indexOf(q),1)
+      }
+    },
+    components:{
+      appQuote: Quote,
+    },
   }
 </script>
 
 <style>
+  @import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+  @import "https://fonts.googleapis.com/css?family=Arizonia";
+
+  .component{
+    margin-top: 30px;
+  }
+  .healthbar {
+    width: 100%;
+    height: 40px;
+    background-color: #eee;
+    transition: width 500ms;
+    border-radius: 4.5px;
+  }
+  .btn-primary {
+    background-color: steelblue;
+  }
 
 </style>
---
+++ vue-cli-ex/src/components/Quote.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="col-lg-3 col-md-4 col-sm-6">
+      <div class="card m-1">
+        <div class="card-body quote" @click="deleteQuote()">
+          <p class="card-text">{{ quote }}</p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      props: ['quote'],
+      methods:{
+        deleteQuote(){
+          this.$emit('deleteQuote',this.quote)
+        }
+      },
+      beforeDestroy(){
+        console.log("this quote was destroyed")
+      }
+    }
+</script>
+
+<style scoped>
+
+  .card-body{
+    font: bold 24px 'Arizonia', cursive;
+    color: #6e6e6e;
+  }
+  .quote {
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+  .quote:hover {
+    background-color: #ffe2e2;
+  }
+</style>
